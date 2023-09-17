09d022ee7fba765fed00783b3ebcc14948da63e3
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-09-17 20:46:06 +0200

    implement jumping with w key

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function animate() {
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'w':
+      if (player.velocity.y === 0) {
+        player.velocity.y = -20
+      } 
+      break
+  }
+})
